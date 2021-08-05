1d6d431ece9a5540e268ff14e5e016cb8cd54e4e
ahSOLO <junmenghan@gmail.com>
2021-08-05 16:36:49 -0700

    add castle building logic

--- Assets/Scripts/BuildingPlacer.cs
+++ Assets/Scripts/BuildingPlacer.cs
@@ -9,16 +9,37 @@ public class BuildingPlacer : MonoBehaviour
 {
     [SerializeField] GameObject buildingPrefab;
     [SerializeField] GameObject environmentContainer;
+    [SerializeField] GameObject selectionCircle;
 
     ARRaycastManager arRM;
     bool buildingIsPlaced;
+    ARPlaneManager arPM;
 
     List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
     private void Start()
     {
         arRM = GetComponent<ARRaycastManager>();
+        arPM = GetComponent<ARPlaneManager>();
         buildingIsPlaced = false;
+    }
+
+    private void Update()
+    {
+        if (arRM.Raycast(new Vector2(Screen.width/2, Screen.height/2), hits, TrackableType.PlaneWithinPolygon) &&
+            buildingIsPlaced == false)
+        {
+            var hitPose = hits[0].pose;
+
+            selectionCircle.SetActive(true);
+            selectionCircle.transform.position = hitPose.position;
+            selectionCircle.transform.rotation = hitPose.rotation;
+        }
+
+        else
+        {
+            selectionCircle.SetActive(false);
+        }
     }
 
     public void PlaceBuilding(InputAction.CallbackContext context)
@@ -31,8 +52,22 @@ public class BuildingPlacer : MonoBehaviour
             var hitPose = hits[0].pose;
 
             Instantiate(buildingPrefab, hitPose.position, hitPose.rotation, environmentContainer.transform);
+            arPM.enabled = false;
+            
             buildingIsPlaced = true;
+
+            DisableARPlanes();
         }
+    }
+
+    public void DisableARPlanes()
+    {
+        foreach (var plane in arPM.trackables)
+        {
+            plane.gameObject.SetActive(false);
+        }
+
+        arPM.enabled = false;
     }
 
 }
